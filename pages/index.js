2fdcbdb099fe11af590bd18337b2e48f90c94514
StarcR <starc.rudi@gmail.com>
2021-04-22 20:38:18 +0200

    message

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,6 @@
 import DropdownExample from '../components/DropdownExample';
 import ButtonExample from '../components/ButtonExample';
-import FormExampleSubmitText from '../components/FormExpampleSubmitText';
+import FormExampleSubmitText from '../components/FormExampleSubmitText';
 import'semantic-ui-css/semantic.min.css';
 
 export default function Home() {
